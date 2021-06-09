4f79cb152938264902b5275e707640106393f2f2
r3nanp <renanmol87@gmail.com>
2021-06-09 19:46:41 -0300

    feat: create resolvers

--- src/modules/products/graphql/resolvers/product.ts
+++ src/modules/products/graphql/resolvers/product.ts
@@ -1,16 +1,47 @@
 import { prisma } from '@shared/prisma/connect'
 
+interface ProductParams {
+  id: string
+}
+
+type CreateProduct = {
+  data: {
+    id: string
+    title: string
+    price: number
+    quantity: number
+  }
+}
+
+async function getProducts() {
+  const product = await prisma.product.findMany()
+
+  return product
+}
+
+async function getProduct({ id }: ProductParams) {
+  const products = await prisma.product.findUnique({
+    where: {
+      id
+    },
+  })
+  return products
+}
+
+async function createProduct({ data }: CreateProduct) {
+  const products = await prisma.product.create({
+    data,
+  })
+
+  return products
+}
+
 export const resolvers = {
   Query: {
-    products: () => {
-      return prisma.product.findMany()
-    },
-    product: (id: number) => {
-      return prisma.product.findUnique({
-        where: {
-          id,
-        },
-      })
-    },
+    products: getProducts,
+    product: getProduct,
+  },
+  Mutation: {
+    createProduct,
   },
 }
